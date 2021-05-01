0176be8b6782c885ecadfbc51b30b4ec01353d77
walker <2303061691@qq.com>
2021-05-01 17:48:15 +0800

    添加单元测试

---
+++ DBAssistant/DBTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DBTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DBTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22f3b8ce-58f3-4ea3-a8e6-ff036427665e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DBAssistant/DBTest/XMLHelperTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WalkerCommon;
+
+namespace DBTest
+{
+    [TestClass]
+    public class XMLHelperTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            XMLHelper.CreatXmlTree(AppDomain.CurrentDomain.BaseDirectory+"xmltest.xml");
+        }
+    }
+}
